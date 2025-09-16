2a2a930e421bc19cdec027d812bc860933c313a3
Azkal-AA <indonesia4b@gmail.com>
2025-09-16 11:13:55 +0700

    feat: Integrasi Laravel Breeze untuk login pengguna

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\BorrowRoomApiController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Di sini Anda dapat mendaftarkan rute web untuk aplikasi Anda.
-| Rute-rute ini dimuat oleh RouteServiceProvider dalam grup yang
-| berisi middleware "web". Buat sesuatu yang hebat!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -25,3 +26,15 @@ Route::group([
 ], function (Router $router) {
     $router->post('borrow-room-with-pegawai', [BorrowRoomApiController::class, 'storeBorrowRoomWithPegawai'])->name('borrow-room-with-pegawai');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
